e795d3bec38b44e93d5933b5573d660977454c4f
MaskoMasko <persic.massimo@gmail.com>
2021-08-15 22:29:17 +0200

    what is going on

--- App.tsx
+++ App.tsx
@@ -1,6 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDhTGx9dL0AMkoRO4FxyuR52yfHOXtDTr0",
+  authDomain: "firebasics-f12b2.firebaseapp.com",
+  projectId: "firebasics-f12b2",
+  storageBucket: "firebasics-f12b2.appspot.com",
+  messagingSenderId: "700695226634",
+  appId: "1:700695226634:web:8f3c83a24f1a2459a2b2f3",
+  measurementId: "G-TCDPQDXBKY",
+};
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
   return (
@@ -14,8 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
